8a3387a01944f4ceea21dfe33baa96cbbe4adb82
user <berezovskievgenisergeevich@mail.ru>
2020-05-25 17:55:41 +0300

    Merge branch 'dev_spring_data_jpa' of C:\Users\user\IdeaProjects\books-library with conflicts.

---
+++ src/main/java/com/berezouski/bookslibrary/service/comments/AddCommentUtils.java
@@ -0,0 +1,24 @@
+package com.berezouski.bookslibrary.service.comments;
+
+import com.berezouski.bookslibrary.domain.Book;
+import com.berezouski.bookslibrary.domain.Entity;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
+
+@SuppressWarnings("JpaQlInspection")
+@Repository
+@Transactional
+public class AddCommentUtils {
+    @PersistenceContext
+    private EntityManager em;
+
+    public void addComment(Book book, String comment) {
+        book.setComment(comment);
+        em.merge(book);
+        em.flush();
+    }
+}
