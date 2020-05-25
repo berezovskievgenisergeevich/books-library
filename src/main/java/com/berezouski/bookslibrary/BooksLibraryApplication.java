b1e9c35b333171e0ead71aa8bc919727eaaf0ae0
user <berezovskievgenisergeevich@mail.ru>
2020-05-25 17:58:55 +0300

    test

--- src/main/java/com/berezouski/bookslibrary/BooksLibraryApplication.java
+++ src/main/java/com/berezouski/bookslibrary/BooksLibraryApplication.java
@@ -3,6 +3,9 @@ package com.berezouski.bookslibrary;
 import com.berezouski.bookslibrary.dao.AuthorDao;
 import com.berezouski.bookslibrary.dao.BookDao;
 import com.berezouski.bookslibrary.domain.Author;
+import com.berezouski.bookslibrary.domain.Entity;
+import com.berezouski.bookslibrary.service.utils.BookService;
+import com.berezouski.bookslibrary.service.utils.EntityService;
 import org.h2.tools.Console;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -15,23 +18,8 @@ public class BooksLibraryApplication {
 
     public static void main(String[] args) throws SQLException {
         ApplicationContext context = SpringApplication.run(BooksLibraryApplication.class, args);
-        System.out.println("Hello World");
 
-        AuthorDao authorDao = context.getBean(AuthorDao.class);
-        BookDao bookDao = context.getBean(BookDao.class);
-
-        System.out.println("All count AUT " + authorDao.count());
-        System.out.println("All count BOOK " + bookDao.count());
-
-        authorDao.printAll();
-        authorDao.insert(new Author(2,"Лев Толстой"));
-        authorDao.printAll();
-        authorDao.deleteById(2);
-        authorDao.printAll();
-
-
-        bookDao.getAll().forEach(System.out::println);
-
+        BookService bookService = context.getBean(BookService.class);
 
         Console.main(args);
     }
